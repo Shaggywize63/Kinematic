24b1d9e2c44d02847bdcac7e71950a223e8e48a6
Sagar Bhargava <sagar12591@hotmail.com>
2026-03-16 19:04:15 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -41,11 +41,13 @@ const allowedOrigins = [
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-   if (allowedOrigins.includes(origin)) {
-  return callback(null, true);
-}
 
-return callback(new Error('Not allowed by CORS'));
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
   credentials: true,
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
